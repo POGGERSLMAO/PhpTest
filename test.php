6732682eaa408596d923979af8a473490ac0cd93
POGGERSLMAO <elliot.palmer@elev.ga.ntig.se>
2019-08-29 10:51:28 +0200

    Second Commit

--- test.php
+++ test.php
@@ -11,6 +11,14 @@
         $name ="Elliot";
 
         echo "<h1> Hello $name!</h1>";
+        $number1 = 21;
+        $number2 = (int) "69";
+        $number3 = false;
+        $number4 = (int) 213.253;
+        $number5 = null;
+
+
+        echo $number1 + $number2 + $number3 + $number4; + $number5;
 
     ?>
 </body>
